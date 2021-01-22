83de848bd5b06da4b430d570f98e067fbad7228a
Victor Menezes <victormenezes35@hotmail.com>
2021-01-21 23:03:12 -0300

    adicionado funcoes de login

--- app/src/main/java/com/victormenezes/whatsapp/activity/RegisterActivity.java
+++ app/src/main/java/com/victormenezes/whatsapp/activity/RegisterActivity.java
@@ -1,12 +1,20 @@
 package com.victormenezes.whatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.victormenezes.whatsapp.R;
+import com.victormenezes.whatsapp.config.ConfigFirebase;
 import com.victormenezes.whatsapp.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -15,6 +23,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText editRegisterEmail;
     private EditText editRegisterPassword;
     private User user;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         editRegisterName = findViewById(R.id.editRegisterName);
         editRegisterEmail = findViewById(R.id.editRegisterEmail);
-        editRegisterPassword = findViewById(R.id.editLoginPassword);
+        editRegisterPassword = findViewById(R.id.editRegisterPassword);
     }
 
     public void register(View view){
@@ -35,7 +44,34 @@ public class RegisterActivity extends AppCompatActivity {
         user.setName(name);
         user.setEmail(email);
         user.setPassword(password);
+        RegisterFirebase();
 
+    }
 
+    private void RegisterFirebase(){
+        auth = ConfigFirebase.getFirebaseAuthentication();
+        auth.createUserWithEmailAndPassword(
+                user.getEmail(),
+                user.getPassword()
+        ).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Sucesso ao cadastrar usuário", Toast.LENGTH_LONG)
+                            .show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Erro ao cadastrar usuário", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        }).addOnFailureListener(RegisterActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Erro ao se conectar com servidor", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
--- app/src/main/java/com/victormenezes/whatsapp/config/ConfigFirebase.java
+++ app/src/main/java/com/victormenezes/whatsapp/config/ConfigFirebase.java
@@ -1,10 +1,13 @@
 package com.victormenezes.whatsapp.config;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public final class ConfigFirebase {
     private static DatabaseReference reference;
+    private static FirebaseAuth auth;
+
 
     public static DatabaseReference getFirebase(){
 
@@ -13,5 +16,12 @@ public final class ConfigFirebase {
         }
 
         return reference;
+    }
+
+    public static FirebaseAuth getFirebaseAuthentication(){
+        if(auth == null){
+            auth = FirebaseAuth.getInstance();
+        }
+        return auth;
     }
 }
